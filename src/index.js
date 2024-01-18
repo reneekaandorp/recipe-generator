16bb5dbe291ca0ce60025166f737c007abe07731
reneekaandorp <127418767+reneekaandorp@users.noreply.github.com>
2024-01-18 10:53:27 +0100

    Added basic form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+function generateRecipe(event) {
+  event.preventDefault();
+  let recipeElement = document.querySelector("#generated-recipe");
+  recipeElement.innerHTML = "The best recipe in the world";
+}
 
+let recipeForm = document.querySelector("#recipe-searcher");
+recipeForm.addEventListener("submit", generateRecipe);
