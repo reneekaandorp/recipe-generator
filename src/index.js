c1240e0047c6f01f1041c2280c5adf824b6f48be
reneekaandorp <127418767+reneekaandorp@users.noreply.github.com>
2024-01-18 12:01:40 +0100

    Started API integration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
+function displayRecipe(response) {
+  console.log(`${response.data.answer}`);
+}
+
 function generateRecipe(event) {
   event.preventDefault();
-  let recipeElement = document.querySelector("#generated-recipe");
-  recipeElement.innerHTML = "The best recipe in the world";
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "79t19ca06b3618febf143dc04f0o86be";
+  let context =
+    "You are an AI culinary expert that likes picking out recipes. Please provide recipes that are simple and delicious";
+  let prompt = `Please generate a recipe with ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(`${prompt}`);
+  console.log(`${context}`);
+
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeForm = document.querySelector("#recipe-searcher");
