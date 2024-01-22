db1730ae4262e1e83f7737daaaaf4cb893e37571
reneekaandorp <127418767+reneekaandorp@users.noreply.github.com>
2024-01-22 14:05:51 +0100

    Improved the UX

--- src/index.js
+++ src/index.js
@@ -9,9 +9,13 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "79t19ca06b3618febf143dc04f0o86be";
   let context =
-    "You are an AI culinary expert that likes picking out recipes. Please provide recipes that are simple and delicious. Please use the following format. <h2>Pancakes</h2> <h3>Ingredients</h3> <ul><li>flour</li><li>eggs</li></ul> <h3>Instructions</h3> <ol><li>Combine the ingredients</li><li>Heat a nonstick pan</li></ol>";
+    "You are an AI culinary expert that likes picking out recipes. Please provide recipes that are simple and delicious. Please use the following format, without additional text. <h2>Pancakes</h2> <h3>Ingredients</h3> <ul><li>flour</li><li>eggs</li></ul> <h3>Instructions</h3> <ol><li>Combine the ingredients</li> <li>Heat a nonstick pan</li> </ol>";
   let prompt = `Please generate a recipe with ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#generated-recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<p class="generating"> Generating your recipe...</p>`;
 
   console.log(`${prompt}`);
   console.log(`${context}`);
